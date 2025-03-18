24f0e0cbd830fb984fc5f468b32027be37eee5cb
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-18 15:28:13 -0300

    Add project files.

---
+++ ClassificaçãoDeTriangulos.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassificaçãoDeTriangulos.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
