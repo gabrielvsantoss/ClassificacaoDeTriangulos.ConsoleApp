528cb4ecc63b5cd5b37f310e0e9b36718fffbf9a
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-18 16:38:49 -0300

    Sequencial Finalizada

--- ClassificaçãoDeTriangulos.ConsoleApp/Program.cs
+++ ClassificaçãoDeTriangulos.ConsoleApp/Program.cs
@@ -12,23 +12,87 @@
                 Console.WriteLine("Classificao de Triangulos");
                 Console.WriteLine("---------------------------------------");
 
+                int X;
+                int Y;
+                int Z;
+                // tryparse é tentar converter
+                while(true)
+                {
+                    Console.WriteLine("Informe o valor de X");
+                    bool ConseguiuConverter = int.TryParse(Console.ReadLine(), out X);
 
-                Console.WriteLine("Informe o valor de X");
-                int X = Convert.ToInt32(Console.ReadLine());
+                    if (ConseguiuConverter)
+                        break;
+                }
 
-                Console.WriteLine("Informe o valor de Y");
-                int Y = Convert.ToInt32(Console.ReadLine());
+                while(true)
+                {
+                    Console.WriteLine("Informe o valor de Y");
+                    bool ConseguiuConverter = int.TryParse(Console.ReadLine(), out Y);
 
-                Console.WriteLine("Informe o valor de Z");
-                int Z = Convert.ToInt32(Console.ReadLine());
+                    if (ConseguiuConverter)
+                        break;
+                }
+
+                while(true)
+                {
+                    Console.WriteLine("Informe o valor de Z");
+                    bool ConseguiuConverter = int.TryParse(Console.ReadLine(), out Z);
+
+                    if (ConseguiuConverter)
+                        break;
+                }
+
+
+
+
+                
 
                 Console.WriteLine("---------------------------------------");
                 Console.WriteLine("Valores Digitados");
                 Console.WriteLine($"Lado X: {X}");
-                Console.WriteLine($"Lado Y: {X}");
-                Console.WriteLine($"Lado Z: {X}");
+                Console.WriteLine($"Lado Y: {Y}");
+                Console.WriteLine($"Lado Z: {Z}");
                 Console.WriteLine("---------------------------------------");
 
+                // Validacao Triangulo
+                bool MedidasValidas = X + Y > Z && X + Z > Y && Y + Z > X;
+
+
+                if (MedidasValidas == true)
+                {
+                    bool trianguloEquilatero = X == Z && X == Y;
+                    bool trianguloEscaleno = X != Z && X != Y && Y != Z;
+
+                    Console.WriteLine("Triangulo Valido");
+                    string triangulo;
+                    if(trianguloEquilatero)
+                    {
+                        triangulo = "Equilatero";
+                    }
+
+                  
+                    else if (trianguloEscaleno)
+                    {
+                        triangulo = "Escaleno";
+                    }
+
+                    else 
+                    {
+                        triangulo = "Isosceles";
+                    }
+
+                    Console.WriteLine($"O triangulo é {triangulo} \nTecle ENTER para continuar");
+                    Console.ReadLine();
+
+
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Os valores Informados não formam um triangulo valido");
+                }
 
 
 
@@ -41,7 +105,7 @@
 
 
 
-                Console.WriteLine("Deseja Continuar (S/N)");
+                    Console.WriteLine("Deseja Continuar (S/N)");
                 string continuar = Console.ReadLine()!.ToUpper();
             
 
