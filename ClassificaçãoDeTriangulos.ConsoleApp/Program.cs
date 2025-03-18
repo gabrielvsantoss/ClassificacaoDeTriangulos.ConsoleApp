e864df7def77026cd800ee5267b4f9d27fbc72b0
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-18 15:39:38 -0300

    Implementada Versao Sequencial

--- ClassificaçãoDeTriangulos.ConsoleApp/Program.cs
+++ ClassificaçãoDeTriangulos.ConsoleApp/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("Classificao de Triangulos");
+                Console.WriteLine("---------------------------------------");
+
+
+                Console.WriteLine("Informe o valor de X");
+                int X = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Informe o valor de Y");
+                int Y = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Informe o valor de Z");
+                int Z = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("Valores Digitados");
+                Console.WriteLine($"Lado X: {X}");
+                Console.WriteLine($"Lado Y: {X}");
+                Console.WriteLine($"Lado Z: {X}");
+                Console.WriteLine("---------------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+                Console.WriteLine("Deseja Continuar (S/N)");
+                string continuar = Console.ReadLine()!.ToUpper();
+            
+
+                if (continuar != "S")
+                {
+                    break;
+                }
+            }
         }
     }
 }
